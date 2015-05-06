e9201d86f38af3747a44b37ae39986db51806e3e
wi13b005 <susannehofmann@gmx.at>
2015-05-06 23:18:35 +0200

    sync changes

--- CS4_secondTry/CS4_secondTry/App.xaml.cs
+++ CS4_secondTry/CS4_secondTry/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace WpfApplication1
+namespace CS4_secondTry
 {
     /// <summary>
     /// Interaction logic for App.xaml
