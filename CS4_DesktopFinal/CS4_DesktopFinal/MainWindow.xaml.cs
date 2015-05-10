e7348c08c25a203f649b4e87c82e9f8d24462236
wi13b005 <susannehofmann@gmx.at>
2015-05-10 14:17:21 +0200

    testdata

--- CS4_DesktopFinal/CS4_DesktopFinal/MainWindow.xaml.cs
+++ CS4_DesktopFinal/CS4_DesktopFinal/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace CS4_DesktopFinal
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
