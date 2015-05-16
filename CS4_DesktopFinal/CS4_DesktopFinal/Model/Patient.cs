e932be2c3971f19095371b22d8a7026e4d7c19b3
wi13b005 <susannehofmann@gmx.at>
2015-05-16 15:54:36 +0200

    c

---
+++ CS4_DesktopFinal/CS4_DesktopFinal/Model/Patient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS4_DesktopFinal.Model
+{
+    class Patient
+    {
+
+        private string firstname;
+        private string lastname;
+        private int ssn;
+
+        public string Firstname {get; set;}
+        public string Lastname {get; set;}
+
+        public int Ssn { get; set; }
+
+
+    }
+}
