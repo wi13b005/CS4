6384fa527708c2d259cce1f6f954bec4fab57863
Roman <roman.schandl@gmail.com>
2015-06-03 23:35:21 +0200

    sync

---
+++ CS4_DesktopFinal/Database/Tables/Line_Item.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Line_Item]
+(
+	[D_line_item] INT NOT NULL PRIMARY KEY, 
+    [F_line_item] INT NULL, 
+    [B_line_item] INT NULL, 
+    [D_product_id] INT NULL, 
+    [D_order_id] INT NULL, 
+    [quantity] INT NULL
+)
