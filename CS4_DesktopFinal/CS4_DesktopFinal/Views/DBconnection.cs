25479d74fa4dca865dd2895e9a4bd186d8eaa409
SaraFH <wi13b169@gmail.com>
2015-06-04 19:34:42 +0200

    DB connection class

---
+++ CS4_DesktopFinal/CS4_DesktopFinal/Views/DBconnection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace CS4_DesktopFinal.Views
+{
+    
+    public class DBconnection
+    {
+
+        public SqlConnection con;
+        public string constr;
+        public void connection()
+        {
+            //DataBase Connection Details  
+            constr = "Data Source=VITHAL;Initial Catalog=C#corner;User Id=sa;Password=swift";
+            con = new SqlConnection(constr);
+            con.Open();
+
+        } 
+
+    }
+}
