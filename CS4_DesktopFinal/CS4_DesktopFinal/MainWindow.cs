2193bce36f2fd936306dfd981469a8cbcbcbfee3
SaraFH <wi13b169@gmail.com>
2015-06-07 23:33:07 +0200

    latest update sara

---
+++ CS4_DesktopFinal/CS4_DesktopFinal/MainWindow.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CS4_DesktopFinal
+{
+    public class MainWindow : Window
+    {
+
+        #region Members
+
+        private TestViewModel _vm;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindow"/> class.
+        /// </summary>
+        public MainWindow()
+        {
+            // Get and set VM
+            _vm = new TestViewModel();
+            this.DataContext = _vm;
+
+            // Initialize UI
+            InitializeComponent();
+        }
+
+        #endregion
+
+
+
+    }
+}
